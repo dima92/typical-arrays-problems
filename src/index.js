b652eaf5e3066442e510d60c37a23e1e37ddafb8
dima92 <dima-tkachenko@mail.ru>
2021-07-01 02:49:26 +0300

    feat: ended TypicalArraysProblems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array && array.length > 0) {
+        return Math.min(...array)
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array && array.length > 0) {
+        return Math.max(...array)
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array && array.length > 0) {
+        let sum = array.reduce((prev, current) => {
+            return prev + current
+        })
+        return sum / array.length
+    } else {
+        return 0;
+    }
 }
